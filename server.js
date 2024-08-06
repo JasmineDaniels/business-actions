1ce5d44777559f898cdf375db7eb494932ddbd15
Jasmine Daniels <create.jasminedaniels@gmail.com>
2024-08-05 20:26:50 -0400

    test new environment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const {PORT = 3000} = process.env;
+const {PORT = 5000} = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
